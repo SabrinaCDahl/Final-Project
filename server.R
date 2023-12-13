dfcc980fce6020467df9c19e43f346abcac9ea72
Sabrina <scdahl@ncsu.edu>
2023-12-12 20:55:10 -0500

    update code

--- server.R
+++ server.R
@@ -23,36 +23,6 @@ shinyServer(function(input, output, session) {
     }
   })
   
-  #creating subtabs within the Modeling tab
-  output$Modeling <- renderUI({
-    tabsetPanel(id="subTabPanel",
-                #subtab describing the two model approaches
-                tabPanel("Modeling Info", 
-                         h3("model one and drawback"),
-                         h3("random forest model and drawback"),
-                         h3("On the following tab there will be a section for inputing the wanted predictors and their interactions for each model. The user will be able to choose from
-                            sexo (Gender), edad (Age), concentracion_hemoglobina(Hemoglobin concentration), temperatura_ambiente (Ambient Temperature), valor_acido_urico (Uric Acid Value), valor_albumina(Albumina Value), valor_colesterol_hdl(HDL Cholesterol Value), valor_colesterol_ldl(LDL CHolesterol Level), valor_colesterol_total(Total Cholesterol Level), valor_creatina(Creatinine level), valor_insulina(Insulin level), valor_trigliceridos(Trigliceride Level), resultado_glucosa (Glucose Result), valor_proteinac_reactiva (C-Reactive Protein Value), resultado_glucosa_promedio (Average Glucose Result), valor_ferritina (Ferritin Value), valor_folato (Folate Value), valor_homocisteina (Homocysteine Value), valor_transferrina (Transferrin Value), valor_vitamina_bdoce (Vitamin B12 Value), valor_vitamina_d (Vitamin D Value), peso (Weight), estatura (Height), tension_arterial (Blood Pressure), sueno_horas (Sleep in Hours), masa_corporal (Body Mass), and actividad_total (Total Activity).")),
-                #subtab where user can change pieces of the model
-                tabPanel("Model Fitting", 
-                         fluidRow(column(4,
-                                         textInput("rfPred", label= "Insert Random Forest Predictors", value = "peso+edad"),
-                                         textInput("glmPred", label= "Insert GLM Predictors", value = "peso+edad"),
-                                         sliderInput("split", label = "Percentage split for train/test sets", value=.7, min=0, max=1),
-                                         sliderInput("cvNum", label = "Number of Cross Validation", value=5, min=2, max=10),
-                                         sliderInput("mtry", label = "Number of input features for random forest model", value = 3, min = 1, max=25),
-                                         actionButton("action", label="Fit Models to training data")
-                                         ),
-                                  column(8,
-                                         uiOutput("models")
-                                         )
-                                  )),
-                #subtab where user can choose values of the predictors
-                tabPanel("Prediction", 
-                         h3("Below is the summary of the each models predictions made using the test set:"),
-                         uiOutput("predictions"))
-    )
-  })
-  
   #develop binary variable of riesgo_hipertension
   hypertension$hyp_binary <- as_factor(ifelse(hypertension$riesgo_hipertension==0, "not_at_risk", "at_risk"))
   
@@ -63,6 +33,7 @@ shinyServer(function(input, output, session) {
     #create training/test set based on percentage split chosen by user
     hypertension.train <- hypertension[train.index,]
     hypertension.test <- hypertension[-train.index,]
+    
     #random forest model
     rf.fit <- train(hyp_binary ~ ., data = hypertension.train,
           #select rf method
@@ -81,16 +52,14 @@ shinyServer(function(input, output, session) {
             trControl = trainControl(method = "cv", number = input$cvNum)
       )
     #calculate RMSE
-    print(rf.fit$results)
-    print(rf.fit$bestTune)
+    RMSE <- renderPrint({
+      rf.fit$results
+      rf.fit$bestTune
     
-    print(glm.fit$results)
-    print(glm.fit$bestTune)
-    #create predictions based off models
-    pred.rf <- predict(rf.fit, newdata=hypertension.test)
-    postResample(pred.rf, hypertension.test$hyp_binary)
-    pred.glm <- predict(glm.fit, newdata=hypertension.test)
-    postResample(pred.glm, hypertension.test$hyp_binary)
+      glm.fit$results
+      glm.fit$bestTune
+    })
+    return(RMSE)
     })
   
   #develop predictions based on models developed on Model Fitting subtab
@@ -119,10 +88,13 @@ shinyServer(function(input, output, session) {
     )
 
     #create predictions based off models
+    preds <- renderPrint({
     pred.rf <- predict(rf.fit, newdata=hypertension.test)
     postResample(pred.rf, hypertension.test$hyp_binary)
     pred.glm <- predict(glm.fit, newdata=hypertension.test)
     postResample(pred.glm, hypertension.test$hyp_binary)
+    })
+    return(preds)
   })
 }
 )
--- ui.R
+++ ui.R
@@ -5,11 +5,13 @@ library(shinydashboard)
 
 shinyUI(fluidPage(
 mainPanel(
-  tabsetPanel(id="tabset",
+  tabsetPanel(id="tabs",
     #tab for describing the app
     tabPanel(title="About", h4("The purpose of this app is to create two models and then compare how well each of those models were able to predict whether an indiviudal will be at risk or not at risk of hypertension."),
              h4("We will be using the data from ", a("kaggle", href="https://www.kaggle.com/datasets/frederickfelix/hipertensin-arterial-mxico"), ". This dataset was collected from the National Health and Nutrition Survey. The dataset includes biometric information about the health of patients in Mexico including Sex, Age, Hemoglobin Concentration, Cholesterol, weight, height, and whether they're risk of developing arterial hypertension."),
              h4("The app has 3 main tabs- About, Data Exploration, and Modeling. You are currently on the About tab describing the purpose of this app. The Data Exploration tab will allow the user to create multiple plots from the dataset to explore any trends and get comfortable with the data. The Modeling tab will develop two different models that will predict an individuals risk factor for developing arterial hypertension."),
+             h5("On the Data Exploration tab and the Model Fitting subtab there will be a section for inputing the wanted variables for plotting/predictors and their interactions for each model. The user will be able to choose from
+                            sexo (Gender), edad (Age), concentracion_hemoglobina(Hemoglobin concentration), temperatura_ambiente (Ambient Temperature), valor_acido_urico (Uric Acid Value), valor_albumina(Albumina Value), valor_colesterol_hdl(HDL Cholesterol Value), valor_colesterol_ldl(LDL CHolesterol Level), valor_colesterol_total(Total Cholesterol Level), valor_creatina(Creatinine level), valor_insulina(Insulin level), valor_trigliceridos(Trigliceride Level), resultado_glucosa (Glucose Result), valor_proteinac_reactiva (C-Reactive Protein Value), resultado_glucosa_promedio (Average Glucose Result), valor_ferritina (Ferritin Value), valor_folato (Folate Value), valor_homocisteina (Homocysteine Value), valor_transferrina (Transferrin Value), valor_vitamina_bdoce (Vitamin B12 Value), valor_vitamina_d (Vitamin D Value), peso (Weight), estatura (Height), tension_arterial (Blood Pressure), sueno_horas (Sleep in Hours), masa_corporal (Body Mass), and actividad_total (Total Activity)."),
              tags$figure(
                class="centerFigure",
                tags$image(
@@ -29,6 +31,30 @@ mainPanel(
              ))
              ),
     #tab for developing models from the dataset
-    tabPanel(title="Modeling", uiOutput("Modeling"))
+    tabPanel(title="Modeling", tabsetPanel(
+      #creating subtabs within the Modeling tab
+      tabPanel("Modeling Info", 
+               h3("model one and drawback"),
+               h3("random forest model and drawback"),
+               ),
+      #subtab where user can change pieces of the model
+      tabPanel("Model Fitting", 
+               fluidRow(column(4,
+                               textInput("rfPred", label= "Insert Random Forest Predictors", value = "peso+edad"),
+                               textInput("glmPred", label= "Insert GLM Predictors", value = "peso+edad"),
+                               sliderInput("split", label = "Percentage split for train/test sets", value=.7, min=0, max=1),
+                               sliderInput("cvNum", label = "Number of Cross Validation", value=5, min=2, max=10),
+                               sliderInput("mtry", label = "Number of input features for random forest model", value = 3, min = 1, max=25),
+                               actionButton("action", label="Fit Models to training data")
+               ),
+               column(8,
+                      uiOutput("models")
+               )
+               )),
+      #subtab where user can choose values of the predictors
+      tabPanel("Prediction", 
+               h3("Below is the summary of the each models predictions made using the test set:"),
+               uiOutput("predictions"))
+    ))
   )
 )))
